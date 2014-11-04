4a0014af96a1d62185acd4b2715b7eb5ba16a50b
Scott Burbidge <scottisreally@gmail.com>
2014-11-03 21:32:35 -0800

    added jscs and jshint to gruntfile.js and uploaded screenshot of code passing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-simple-mocha');
-
+  grunt.loadNpmTasks('grunt-contrib-jscs')
   grunt.initConfig({
     jshint: {
       all: ["lib/*.js"]
@@ -9,8 +9,20 @@ module.exports = function(grunt) {
 
     simplemocha: {
       src: ['test/**/*.js']
+    },
+
+    jscs: {
+      all: {
+        files: {
+          src: ["lib/mean_median_mode.js"]
+        },
+        options: {
+          "standards": "idiomatic"
+        }
+      }
     }
   });
-  grunt.registerTask('test', ['jshint', 'simplemocha']);
+  grunt.registerTask('test', ['jshint', 'simplemocha', 'jscs']);
   grunt.registerTask('default', ['test']);
-}
+};
+
